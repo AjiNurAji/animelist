6726f5d5586532434f606cd3c52308b5b2a1ce14
AjiNurAji <ajinuraji090306@gmail.com>
2023-11-19 13:59:47 +0700

    tugas sesi 4 validate pagination

--- src/components/Utilities/Pagination.jsx
+++ src/components/Utilities/Pagination.jsx
@@ -1,29 +1,48 @@
 const Pagination = ({ page, lastPage, setPage }) => {
-
   const scrollTop = () => {
     scrollTo({
-      behavior: 'smooth',
-      top: 0
-    })
-  }
+      behavior: "smooth",
+      top: 0,
+    });
+  };
 
   const handleNextPage = () => {
-    setPage((prevState) => prevState + 1)
-    scrollTop()
-  }
+    setPage((prevState) => prevState + 1);
+    scrollTop();
+  };
 
   const handlePrevPage = () => {
-    setPage((prevState) => prevState - 1)
-    scrollTop()
-  }
+    setPage((prevState) => prevState - 1);
+    scrollTop();
+  };
 
   return (
     <div className="flex justify-center items-center py-4 px-2 gap-4 text-color-primary text-2xl">
-      <button className="transition-all hover:text-color-accent" onClick={handlePrevPage}>Prev</button>
-      <p>{page} of {lastPage}</p>
-      <button className="transition-all hover:text-color-accent" onClick={handleNextPage}>Next</button>
+      {page === 1 && page != 0 ? (
+        <button className="text-color-secondary cursor-auto">Prev</button>
+      ) : (
+        <button
+          className="transition-all hover:text-color-accent"
+          onClick={handlePrevPage}
+        >
+          Prev
+        </button>
+      )}
+      <p>
+        {page} of {lastPage}
+      </p>
+      {lastPage === page ? (
+        <button className="text-color-secondary cursor-auto">Next</button>
+      ) : (
+        <button
+          className="transition-all hover:text-color-accent"
+          onClick={handleNextPage}
+        >
+          Next
+        </button>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Pagination
+export default Pagination;
